24e3b7c2b4ee708fd4c712680d9222a8ae55ac78
tovdata <dev@tovdata.com>
2022-01-12 09:35:22 +0900

    Initial commit

---
+++ test/v2pc-backend-infra-for-cicd.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as V2PcBackendInfraForCicd from '../lib/v2pc-backend-infra-for-cicd-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/v2pc-backend-infra-for-cicd-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new V2PcBackendInfraForCicd.V2PcBackendInfraForCicdStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
